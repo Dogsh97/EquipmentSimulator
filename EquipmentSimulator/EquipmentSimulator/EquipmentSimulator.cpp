1344c4c5c332c8742e5112579cc438fbddd070a8
Dogsh97 <hoony99@gmail.com>
2026-07-27 16:53:11 +0900

    기능 추가 :  새로운 state machine 추가

---
+++ EquipmentSimulator/EquipmentSimulator/EquipmentSimulator.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "EquipmentController.h"
+
+int main()
+{
+	EquipmentController equipment;
+
+	
+	equipment.PrintState();
+	equipment.Initialize();
+	equipment.PrintState();
+	equipment.CompleteInitialization();
+	equipment.PrintState();
+	equipment.Start();
+	equipment.PrintState();
+	equipment.Complete();
+	equipment.PrintState();
+	equipment.Start();
+	equipment.PrintState();
+	equipment.RaiseError();
+	equipment.PrintState();
+	equipment.Reset();
+	equipment.PrintState();
+
+	return 0;
+}
