96c3ecdf357330ed8d3a70c65ab910d24a4eb3e8
Dogsh97 <hoony99@gmail.com>
2026-07-28 17:13:57 +0900

    Wafer model  구현

---
+++ EquipmentSimulator/EquipmentSimulator/Wafer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+enum class WaferState {
+	EMPTY,
+	LOADED,
+	PROCESSING,
+	COMPLETED
+};
+
+class Wafer {
+	private:
+		int waferId;
+		WaferState state;
+
+	public:
+		Wafer();
+
+		void Load(int id);
+		void StartProcessing();
+		void CompleteProcess();
+		void PrintInfo();
+
+};
