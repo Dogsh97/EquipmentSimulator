1545f4e8bb35e2628a67db2ea48f79b972bb6075
Dogsh97 <hoony99@gmail.com>
2026-08-14 16:27:36 +0900

    EquipmentState  전이 검증

--- EquipmentSimulator/EquipmentSimulator/EquipmentSimulator.cpp
+++ EquipmentSimulator/EquipmentSimulator/EquipmentSimulator.cpp
@@ -101,6 +101,23 @@ int main()
         equipment.PrintEventLogs();
     }
 
+    std::cout << "\n===== Test 6 : Reset State Consistency =====\n";
+
+    EquipmentController equipment;
+
+    equipment.MakeCommand(Command(CommandType::Initialize));
+    equipment.MakeCommand(Command(CommandType::CompleteInitialization));
+    equipment.MakeCommand(Command(CommandType::SetRecipe, 1, 1, 1));
+    equipment.MakeCommand(Command(CommandType::LoadWafer, 1));
+    equipment.MakeCommand(Command(CommandType::Start));
+    equipment.MakeCommand(Command(CommandType::RaiseError));
+    equipment.MakeCommand(Command(CommandType::Reset));
+
+    equipment.RunCommand();
+
+    equipment.PrintState();
+    equipment.PrintEventLogs();
+
     return 0;
 
 }
